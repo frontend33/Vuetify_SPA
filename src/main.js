8e1af6208980e88867e4e23437c4582a47ae8e8f
frontend33 <vikulov33@rambler.ru>
2019-01-07 00:38:23 +0300

    Set up and webpack added library Vuetify

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App'
+import router from './router'
+Vue.config.productionTip = false
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})

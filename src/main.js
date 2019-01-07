caf07591a85bedb3317ea80f6a6dee36781bb48b
frontend33 <vikulov33@rambler.ru>
2019-01-07 16:37:49 +0300

    create template add icons vuetify style

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+Vue.use(Vuetify, {
+  iconfont: 'md'
+})
+Vue.use(Vuetify)
 Vue.config.productionTip = false
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,

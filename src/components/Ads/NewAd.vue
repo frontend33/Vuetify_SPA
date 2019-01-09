8a695f8acd6aa59d7839b4c73fa7d0e5432e7d74
frontend33 <vikulov33@rambler.ru>
2019-01-09 07:00:58 +0300

    create new ads working with text fields

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,63 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>New ad</h1>
+      <v-flex xs12 sm-6 offset-sm3>
+        <h1 class="text-secondary mb-3">Create new ad</h1>
+        <v-form  ref="form" v-model="valid" validation class="mb-3">
+          <v-text-field
+            name="title"
+            label="Ad title"
+            type="text"
+            v-model="title"
+            required
+            :rules="[v=> !!v || 'Title is required']"
+          >
+          </v-text-field>
+          <v-textarea
+            name="description"
+            label="Ad description"
+            type="text"
+            v-model="description"
+
+            :rules="[v=> !!v || 'Description is required']"
+          >
+          </v-textarea>
+        </v-form>
+        <v-layout row mb-3>
+          <v-flex xs12>
+            <v-btn color="warning">
+              Upload
+              <v-icon right dark>cloud_upload</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <img src="" height="100">
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-switch
+              label="Add to promo?"
+              v-model="promo"
+              color="primary"
+            >
+            </v-switch>
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs12>
+            <v-spacer></v-spacer>
+            <v-btn
+            :disabled="!valid"
+              color="success"
+              @click="createAd"
+            >
+            Create ad
+            </v-btn>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +66,24 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      title: '',
+      description: '',
+      promo: false,
+      valid: false
+    }
+  },
+  methods: {
+    createAd () {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        }
+        console.log(ad)
+      }
+    }
   }
 }
 </script>

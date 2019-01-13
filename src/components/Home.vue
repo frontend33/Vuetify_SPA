b5ac7286300ad9d819a5ec1823ac78dd624b4faa
frontend33 <vikulov33@rambler.ru>
2019-01-14 00:26:40 +0300

    the loading of ads from the database

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="!loading">
     <v-container fluid>
       <v-layout row>
         <v-flex xs12>
@@ -48,7 +48,22 @@
         </v-flex>
       </v-layout>
     </v-container>
-    </div>
+  </div>
+  <div v-else>
+    <v-container>
+      <v-layout row> 
+        <v-flex xs12 class="text-xs-center pt-5">
+          <v-progress-circular
+            :size="100"
+            :width="4"
+            color="primary"
+            indeterminate
+          >
+          </v-progress-circular>
+        </v-flex>  
+      </v-layout>
+    </v-container> 
+  </div>
 </template>
 
 <script>
@@ -60,6 +75,9 @@ export default {
     },
     ads () {
       return this.$store.getters.ads
+    },
+    loading () {
+      return this.$store.getters.loading
     }
   }
 }
--- src/store/ads.js
+++ src/store/ads.js
@@ -1,53 +1,27 @@
 import * as fb from 'firebase'
 class Ad {
-  constructor (title, description, ownerId, imgSrc = '', promo = false, id = null) {
+  constructor(title, description, ownerId, imgSrc = '', promo = false, id = null) {
     this.title = title
     this.description = description
     this.ownerId = ownerId
     this.imgSrc = imgSrc
     this.promo = promo
-    // this.id = id
+    this.id = id
   }
 }
 
 export default {
 
   state: {
-    ads: [
-      {
-        title: 'first ad',
-        description: 'Hello i am description',
-        promo: true,
-        imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-        id: '123'
-      },
-      {
-        title: 'first ad',
-        description: 'Hello i am description',
-        promo: true,
-        imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-        id: '1234'
-      },
-      {
-        title: 'first ad',
-        description: 'Hello i am description',
-        promo: false,
-        imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-        id: '12345'
-      },
-      {
-        title: 'first ad',
-        description: 'Hello i am description',
-        promo: false,
-        imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-        id: '12'
-      }
-    ]
+    ads: []
   },
   mutations: {
     createAdMutations (state, payload) {
-      // В объект ads добавляем новое объявление  
+      // В объект ads добавляем новое объявление
       state.ads.push(payload)
+    },
+    loadAds (state, payload) {
+      state.ads = payload
     }
   },
   actions: {
@@ -55,7 +29,7 @@ export default {
     // в actions помимо commit получиаем параметр getters можем получить id текущего пользователя
     async createAdActions ({commit, getters}, payload) {
       commit('clearError')
-      commit('setLoading',true)
+      commit('setLoading', true)
       try {
         const newAd = new Ad(
           payload.title,
@@ -68,27 +42,59 @@ export default {
         // что бы создать новый элемент в  бд используем метод push
         // метод идет асинхронно к бд и создает поля
         const ad = await fb.database().ref('ads').push(newAd)
-        commit ('setLoading', false)
-        commit ('createAdMutations', {
+        commit('setLoading', false)
+        commit('createAdMutations', {
           // Расширим объект newAd еще одним ключом при помощи spread оператора
           ...newAd,
           id: ad.key
         })
       } catch (error) {
-        commit ("setError", error.message)
-        commit ('setLoading', false)
+        commit('setError', error.message)
+        commit('setLoading', false)
         throw error
         // и выкидываем ошибку что бы обработать в промисе
       }
 
       // payload.id = Math.random().toString()
       // commit('createAdMutations', payload)
+    },
+    async fetchAds ({commit}) {
+      commit('clearError')
+      commit('setLoading', true)
+      const resultAds = []
+      try {
+        // Указываем таблицу и метод который заберет все данные из таблицы once
+        const fbVal = await fb.database().ref('ads').once('value')
+        // Получить все значения у объекта
+        const ads = fbVal.val()
+        // Пробежимся по всем ключам объекта
+        Object.keys(ads).forEach(key => {
+          const ad = ads[key]
+          resultAds.push(
+            new Ad(
+              ad.title,
+              ad.description,
+              ad.ownerId,
+              ad.imgSrc,
+              ad.promo,
+              key
+            )
+          )
+        })
+        commit('loadAds', resultAds)
+
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
     }
   },
   getters: {
-    ads (state) {
-      return state.ads
-    },
+    // ads (state) {
+    //   return state.ads
+    // },
     promoAds (state) {
       return state.ads.filter(ad => {
         return ad.promo === true
